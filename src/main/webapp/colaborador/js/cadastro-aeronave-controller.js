bf78f53c280df9d9e46a860fc866cf482062d427
Bruno Yudi Tiyoda <bruno_tiyoda@hotmail.com>
2018-05-24 20:39:52 -0300

    Testes

---
+++ src/main/webapp/colaborador/js/cadastro-aeronave-controller.js
@@ -0,0 +1,9 @@
+angular.module('colapp').controller('AeronaveController', function ($scope, $http) {
+
+    console.log("AeroContro");
+
+    $cope.salvar = function () {
+      console.log("salvar");
+    };
+
+});
--- src/main/webapp/colaborador/js/main.js
+++ src/main/webapp/colaborador/js/main.js
@@ -1,1 +1,10 @@
-angular.module('colapp',['ngRoute', 'ngAnimate']);
+angular.module('colapp', ['ngRoute', 'ngAnimate'])
+    .config(function ($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'cadastro_aeronave.html',
+                    controller: 'AeronaveController'
+                });
+
+            //$locationProvider.html5Mode(true);
+        });

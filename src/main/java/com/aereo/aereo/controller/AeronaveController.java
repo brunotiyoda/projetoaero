681e02edd1a4b037f99d80ed432be3914fdf3645
Bruno Yudi Tiyoda <bruno_tiyoda@hotmail.com>
2018-06-04 21:54:04 -0300

    Testes

--- src/main/java/com/aereo/aereo/controller/AeronaveController.java
+++ src/main/java/com/aereo/aereo/controller/AeronaveController.java
@@ -7,6 +7,7 @@ import com.aereo.aereo.service.AeronaveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -20,5 +21,10 @@ public class AeronaveController extends GenericController<Aeronave> {
     @Override
     public GenericService<Aeronave> getGenericService() {
         return aeronaveService;
+    }
+
+    @RequestMapping(value = "/think", method = RequestMethod.GET)
+    public Aeronave think() {
+        return new Aeronave();
     }
 }
